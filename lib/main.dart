4143e5e72fcee54ba58ed890d05c5527248a16f1
yang zhang <tianna1121@gmail.com>
2019-09-18 13:48:28 +0800

    add logic for calculate the score

--- lib/main.dart
+++ lib/main.dart
@@ -23,11 +23,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   List<ItemModel> items;
   List<ItemModel> items2;
+  int score;
 
   @override
   void initState() {
     super.initState();
     initGame();
+    score = 0;
   }
 
   initGame() {
@@ -58,57 +60,77 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Row(children: <Widget>[
-            Column(
-              children: items.map((item) {
-                return Container(
-                    margin: EdgeInsets.all(8.0),
-                    child: Draggable<ItemModel>(
-                        data: item,
-                        childWhenDragging: Icon(
-                          item.icon,
-                          color: Colors.grey,
-                          size: 50.0,
+          child: Column(
+            children: <Widget>[
+              Text.rich(TextSpan(children: [
+                TextSpan(text: 'Score: '),
+                TextSpan(
+                    text: '$score',
+                    style: TextStyle(
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 30.0))
+              ])),
+              Row(children: <Widget>[
+                Column(
+                  children: items.map((item) {
+                    return Container(
+                        margin: EdgeInsets.all(8.0),
+                        child: Draggable<ItemModel>(
+                            data: item,
+                            childWhenDragging: Icon(
+                              item.icon,
+                              color: Colors.grey,
+                              size: 50.0,
+                            ),
+                            feedback: Icon(
+                              item.icon,
+                              color: Colors.red,
+                              size: 50.0,
+                            ),
+                            child: Icon(item.icon,
+                                color: Colors.red, size: 50.0)));
+                  }).toList(),
+                ),
+                Spacer(),
+                Column(
+                  children: items.map((item) {
+                    return DragTarget<ItemModel>(
+                      onWillAccept: (receivedItem) => true,
+                      onAccept: (receivedItem) {
+                        if (item.value == receivedItem.value) {
+                          setState(() {
+                            items.remove(receivedItem);
+                            items2.remove(item);
+                            score += 10;
+                          });
+                        } else {
+                          setState(() {
+                            score -= 5;
+                          });
+                        }
+                      },
+                      builder: (context, acceptedItems, rejectedItems) =>
+                          Container(
+                        color: Colors.red,
+                        height: 50.0,
+                        width: 100.0,
+                        alignment: Alignment.center,
+                        margin: EdgeInsets.all(8.0),
+                        child: Text(
+                          item.name,
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0),
                         ),
-                        feedback: Icon(
-                          item.icon,
-                          color: Colors.red,
-                          size: 50.0,
-                        ),
-                        child: Icon(item.icon, color: Colors.red, size: 50.0)));
-              }).toList(),
-            ),
-            Spacer(),
-            Column(
-              children: items.map((item) {
-                return DragTarget<ItemModel>(
-                  onWillAccept: (receivedItem) => true,
-                  onAccept: (receivedItem) {
-                    if (item.value == receivedItem.value) {
-                      setState(() {
-                        items.remove(receivedItem);
-                        items2.remove(item);
-                      });
-                    }
-                  },
-                  builder: (context, acceptedItems, rejectedItems) => Container(
-                    color: Colors.red,
-                    height: 50.0,
-                    width: 100.0,
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.all(8.0),
-                    child: Text(
-                      item.name,
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0),
-                    ),
-                  ),
-                );
-              }).toList(),
-            ),
-          ]),
+                      ),
+                    );
+                  }).toList(),
+                ),
+              ]),
+            ],
+          ),
         ),
       ),
     );
